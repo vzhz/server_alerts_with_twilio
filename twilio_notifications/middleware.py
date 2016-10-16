3ffc5fc45c0aa4be31a6ee7d26b867b63e6c73cd
vzhz <vehanus@gmail.com>
2016-10-16 15:41:15 -0400

    add comments to exceptions

--- twilio_notifications/middleware.py
+++ twilio_notifications/middleware.py
@@ -1,3 +1,5 @@
+
+### To send message, create Twilio REST client ###
 def load_twilio_config():
     twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
     twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
@@ -9,16 +11,18 @@ def load_twilio_config():
 
     return (twilio_number, twilio_account_sid, twilio_auth_token)
 
-
+### Error handling with Django middleware. API calls from here ###
 class TwilioNotificationsMiddleware(object):
     def __init__(self):
         self.administrators = load_admins_file()
         self.client = MessageClient()
 
+### Custom alert message to send out via text message ###
     def process_exception(self, request, exception):
         exception_message = str(exception)
         message_to_send = MESSAGE % exception_message
 
+### Trigger motifications for admin list ###
         for admin in self.administrators:
             self.client.send_message(message_to_send, admin['phone_number'])
 
