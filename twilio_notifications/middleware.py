2d0755f7240299b3a0ecb2a47f2fb527119bec9b
vzhz <vehanus@gmail.com>
2016-10-16 15:17:26 -0400

    configure twilio client

---
+++ twilio_notifications/middleware.py
@@ -0,0 +1,10 @@
+def load_twilio_config():
+    twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    twilio_number = os.environ.get('TWILIO_NUMBER')
+
+    if not all([twilio_account_sid, twilio_auth_token, twilio_number]):
+        logger.error(NOT_CONFIGURED_MESSAGE)
+        raise MiddlewareNotUsed
+
+    return (twilio_number, twilio_account_sid, twilio_auth_token)
